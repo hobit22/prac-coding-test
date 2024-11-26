140581d8bcb6bd88125526089a3d1589e53b8c22
Hobin Kim <40729223+hobit22@users.noreply.github.com>
2024-11-26 21:04:43 +0900

    [level 2] Title: 가장 큰 수, Time: 295.35 ms, Memory: 120 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(int[] numbers) {
+        return Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .sorted((s1, s2) -> {
+                    int original = Integer.parseInt(s1 + s2);
+                    int reverse = Integer.parseInt(s2 + s1);
+                    return reverse - original;
+                })
+                .collect(Collectors.joining())
+                .replaceAll("^0+", "0");
+    }
+}
