51271bbd397ef608622342d2851d80cb5d88a23e
Hobin Kim <40729223+hobit22@users.noreply.github.com>
2024-11-28 23:52:17 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.22 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        Set<Character> set = new HashSet<>();
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (char c : my_string.toCharArray()) {
+            if (set.contains(c)) continue;
+            set.add(c);
+            sb.append(c);
+        }
+        
+        return sb.toString();
+    }
+}
