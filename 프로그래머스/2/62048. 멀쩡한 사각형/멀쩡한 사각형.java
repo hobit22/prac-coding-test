3d662f28965a419e903ad7a2679f314f4615e08c
Hobin Kim <40729223+hobit22@users.noreply.github.com>
2025-04-30 17:06:55 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.45 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(int w, int h) {
+        long W = (long) w;
+        long H = (long) h;
+        return W * H - (W + H - gcd(W, H));
+    }
+
+    private long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+}
